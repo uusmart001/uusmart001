549a828c8fc8d403e68203d5cb9477e074c67450
uu-smart <wanb001@hotmail.com>
2026-05-26 21:38:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/uusmart001/',
   lang: 'zh-CN',
   title: 'My Vuepress Site',
   description: '',
